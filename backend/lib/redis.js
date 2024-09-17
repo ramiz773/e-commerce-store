718734c636bc1d1cd7f725e9abb282e4f6f5f801
ramiz773 <aleemrameestpn@gmail.com>
2024-09-17 18:30:26 +0530

    fix redis connection problem re setuped

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,5 +1,7 @@
-import Redis from "ioredis";
-import dotenv from "dotenv";
+import Redis from "ioredis"
+import dotenv from 'dotenv'
 
-dotenv.config();
-export const redis = new Redis(process.env.UPSTASH_REDIS_URI);
+dotenv.config()
+
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+await redis.set('foo', 'bar');
