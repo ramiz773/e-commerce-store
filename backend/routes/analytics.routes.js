0d74058f4c0436a6ae9dad65afb453d8382b42bb
ramiz773 <carrybe9@gmail.com>
2024-10-09 19:43:33 +0530

    bug fix in analytics route

--- backend/routes/analytics.routes.js
+++ backend/routes/analytics.routes.js
@@ -5,18 +5,18 @@ import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.co
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, async (req, res) => {
-   try {
-      const analyticData = await getAnalyticsData();
+  try {
+    const analyticsData = await getAnalyticsData();
 
-      const endDate = new Date();
-      const startDate = new Date(endDate.getTime() - 7 * 24 * 24 * 60 * 60 * 1000);
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
 
-      const dailySalesData = await getDailySalesData(startDate, endDate);
-      res.json({ analyticData, dailySalesData });
-   } catch (error) {
-      console.log(`Error in sales analytic route : ${error.message}`);
-      res.status(500).json({ message: "server error", error: error.message });
-   }
+    const dailySalesData = await getDailySalesData(startDate, endDate);
+    res.json({ analyticsData, dailySalesData });
+  } catch (error) {
+    console.log(`Error in sales analytic route : ${error.message}`);
+    res.status(500).json({ message: "server error", error: error.message });
+  }
 });
 
 export default router;
